71197a8ceb9a5ef2b7fa2561bb2b9e98b3ceeb09
戚耿鑫 <qgx446738721@hotmail.com>
2014-03-11 18:51:57 +0800

    homework

--- 归并排序.cpp
+++ 归并排序.cpp
@@ -9,7 +9,7 @@
 #include<vector>
 #include<queue>
 #include<stack>
-//#pragma comment(linker, "/STACK:102400000,102400000")
+#pragma comment(linker, "/STACK:102400000,102400000")
 #ifndef ONLINE_JUDGE
 #define scanf scanf_s
 #endif
@@ -24,17 +24,17 @@ const int MAX_N = 40005;
 void Sort(int *A, int l, int r){
 	if (r - l + 1 <= 2){
 		if (A[l] > A[r]){
-			int tmp = A[l];
+			int tmp2 = A[l];
 			A[l] = A[r];
-			A[r] = tmp;
+			A[r] = tmp2;
 		}
 		return;
 	}
-	int *tmp = new int[r - l + 1];
 	int mid = (l + r) >> 1;
 	Sort(A, l, mid), Sort(A, mid + 1, r);
+	int *tmp = new int[r - l + 1];
 	int i = l, j = mid + 1;
-	int cnt = l;
+	int cnt = 0;
 	while (i <= mid || j <= r){
 		if (i == mid + 1){
 			tmp[cnt++] = A[j++];
@@ -51,23 +51,23 @@ void Sort(int *A, int l, int r){
 			}
 		}
 	}
-	for (int i = l; i <= r; i++){
-		A[i] = tmp[i];
+	for (i = l, j = 0; i <= r; i++, j++){
+		A[i] = tmp[j];
 	}
+	delete[] tmp;
 }
 int main(){
 	int n;
 	while (cin >> n){
 		int *A = new int[n];
-		for (int i = 1; i <= n; i++){
+		for (int i = 0; i < n; i++){
 			cin >> A[i];
 		}
-		Sort(A, 1, n);
-		for (int i = 1; i <= n; i++){
+		Sort(A, 0, n - 1);
+		for (int i = 0; i < n; i++){
 			cout << A[i] << ' ';
 		}
 		cout << endl;
-		delete[] A;
 	}
 #ifndef ONLINE_JUDGE
 	system("pause");
